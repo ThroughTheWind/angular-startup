997e8e05d0bcca574cf494a044050bbfae949e9d
Ninja_ <antoine.dcr@outlook.fr>
2019-08-27 23:53:39 +0200

    File upload working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FileUploadModule } from './shared/file-upload/file-upload.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -19,7 +20,8 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
